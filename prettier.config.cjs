155d8691ce20a7c7d8f8a2e7beebfa95234d1459
mdarocha <git@mdarocha.pl>
2025-01-25 13:36:17 +0100

    More formatting, editorconfig

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,12 +1,11 @@
 /** @type {import('prettier').Config} */
 module.exports = {
-    plugins: [require.resolve("@trivago/prettier-plugin-sort-imports")],
     semi: true,
     singleQuote: false,
-    tabWidth: 4,
     trailingComma: "all",
-    useTabs: false,
+
     // Import sorting
+    plugins: [require.resolve("@trivago/prettier-plugin-sort-imports")],
     importOrderSeparation: true,
     importOrderSortSpecifiers: true,
 };
