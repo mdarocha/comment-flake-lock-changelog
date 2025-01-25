6861a02137b55175175b0b02b6347470b771528d
mdarocha <git@mdarocha.pl>
2025-01-25 14:31:24 +0100

    devenv enterTest

--- devenv.nix
+++ devenv.nix
@@ -19,4 +19,24 @@
       pass_filenames = false;
     };
   };
+
+  enterTest = ''
+    # Basic checks - format, lint, typescript
+    bun run format:check
+    bun run lint
+    bun run typecheck
+
+    # Tests
+    bun test
+
+    # Check if the dist/ file is up-to-date
+    before_hash=$(sha256sum dist/** | sha256sum | cut -d ' ' -f1)
+    bun run build
+    after_hash=$(sha256sum dist/** | sha256sum | cut -d ' ' -f1)
+
+    if [ "$before_hash" != "$after_hash" ]; then
+      echo "dist/ is not up-to-date"
+      exit 1
+    fi
+  '';
 }
