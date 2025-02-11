d32107b329a573865de25cce27dca4bfae4ac1c3
mdarocha <git@mdarocha.pl>
2025-02-11 20:25:41 +0100

    Add action.yml

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,8 @@
+async function writeResultFile(content: string): Promise<void> {
+    const path = `${process.env.RUNNER_TEMP}/comment-flake-lock-changelog-result.md`;
+    await Bun.write(path, content);
+}
+
 export async function run(): Promise<void> {
     throw new Error("TODO!");
 }
